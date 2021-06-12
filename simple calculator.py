b14219a4ab41c1e8bf90b9a8706c86d3b8ac87f6
Shuhua Shrestha <71085603+shuhua833@users.noreply.github.com>
2021-06-12 16:09:58 +0545

    Create simple calculator.py

---
+++ simple calculator.py
@@ -0,0 +1,15 @@
+num1= input("enter a number\n")
+num2=input("enter another\n")
+
+num1 = int(num1)
+num2 = int(num2)
+
+add= num1+num2
+sub= num1-num2
+mul= num1*num2
+div= num1/num2
+
+print(add)
+print(sub)
+print(mul)
+print(div)
